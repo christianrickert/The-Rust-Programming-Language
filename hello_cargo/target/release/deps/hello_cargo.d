cbf1963e1d223e669053e38e380c382060f3296b
Christian Rickert <rc.email@icloud.com>
2021-09-19 20:15:58 -0600

    Building for Release

---
+++ hello_cargo/target/release/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\Christian Rickert\Documents\Github\The-Rust-Programming-Language\hello_cargo\target\release\deps\hello_cargo.exe: src\main.rs
+
+C:\Users\Christian Rickert\Documents\Github\The-Rust-Programming-Language\hello_cargo\target\release\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\Christian\ Rickert\Documents\Github\The-Rust-Programming-Language\hello_cargo\target\release\hello_cargo.exe: C:\Users\Christian\ Rickert\Documents\Github\The-Rust-Programming-Language\hello_cargo\src\main.rs
