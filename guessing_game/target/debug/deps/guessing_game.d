ad84baa7e5aa142b7122e788b7560a4fb66f1005
Christian Rickert <rc.email@icloud.com>
2021-09-20 20:47:17 -0600

    Setting Up a New Project

---
+++ guessing_game/target/debug/deps/guessing_game.d
@@ -0,0 +1,5 @@
+C:\Users\Christian Rickert\Documents\Github\The-Rust-Programming-Language\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
+
+C:\Users\Christian Rickert\Documents\Github\The-Rust-Programming-Language\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
+
+src\main.rs:
