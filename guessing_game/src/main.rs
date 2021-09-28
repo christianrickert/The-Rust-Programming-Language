85769bd68b4cda503053ed8b7bf85bb5e8897845
Christian Rickert <rc.email@icloud.com>
2021-09-27 21:03:55 -0600

    Handling Invalid Input

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,14 +1,12 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
-    
+
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-    println!("The secret nubmer is: {}", secret_number);
-    
     loop {
         println!("Please input your guess.");
 
@@ -18,9 +16,12 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-        println!("You guess: {}", guess);
+        println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
@@ -32,4 +33,3 @@ fn main() {
         }
     }
 }
-
---
+++ guessing_game/target/release/deps/rand_chacha-cc6161f8b21c009f.d
@@ -0,0 +1,9 @@
+C:\Users\Christian Rickert\Documents\Github\The-Rust-Programming-Language\guessing_game\target\release\deps\rand_chacha-cc6161f8b21c009f.rmeta: C:\Users\Christian\ Rickert\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.1\src\lib.rs C:\Users\Christian\ Rickert\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.1\src\chacha.rs C:\Users\Christian\ Rickert\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.1\src\guts.rs
+
+C:\Users\Christian Rickert\Documents\Github\The-Rust-Programming-Language\guessing_game\target\release\deps\librand_chacha-cc6161f8b21c009f.rlib: C:\Users\Christian\ Rickert\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.1\src\lib.rs C:\Users\Christian\ Rickert\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.1\src\chacha.rs C:\Users\Christian\ Rickert\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.1\src\guts.rs
+
+C:\Users\Christian Rickert\Documents\Github\The-Rust-Programming-Language\guessing_game\target\release\deps\rand_chacha-cc6161f8b21c009f.d: C:\Users\Christian\ Rickert\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.1\src\lib.rs C:\Users\Christian\ Rickert\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.1\src\chacha.rs C:\Users\Christian\ Rickert\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.1\src\guts.rs
+
+C:\Users\Christian\ Rickert\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.1\src\lib.rs:
+C:\Users\Christian\ Rickert\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.1\src\chacha.rs:
+C:\Users\Christian\ Rickert\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.1\src\guts.rs:
