a65e07aef25c4f8b65c8763ddabeb4ebb82c95a9
Christian Rickert <rc.login@icloud.com>
2021-09-27 21:21:22 -0600

    Delete hello_cargo/target directory

--- hello_cargo/target/release/deps/hello_cargo.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Christian Rickert\Documents\Github\The-Rust-Programming-Language\hello_cargo\target\release\deps\hello_cargo.exe: src\main.rs
-
-C:\Users\Christian Rickert\Documents\Github\The-Rust-Programming-Language\hello_cargo\target\release\deps\hello_cargo.d: src\main.rs
-
-src\main.rs:
