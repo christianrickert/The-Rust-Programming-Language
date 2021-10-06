5fe86759eddfee4faf7967eda3aa60328183e0d3
Christian Rickert <rc.email@icloud.com>
2021-10-05 20:19:02 -0600

    Listing 4-4

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,23 +1,26 @@
 fn main() {
-    let s = String::from("hello");  // s comes into scope
+    let s1 = gives_ownership();         // gives_ownership moves its return
+                                        // value into s1
 
-    takes_ownership(s);             // value of s moves into the function...
-                                    // ... and s is thus invalid here
+    let s2 = String::from("hello");     // s2 comes into scope
 
-    let x = 5;                      // x comes into scope
+    let s3 = takes_and_gives_back(s2);  // s2 is moved into takes_and_gives_back,
+                                        // which also moves its return value into s3
+}   // Here, s3 and s1 go out of scope
+    // are dropped. s2 goes out of scope
+    // but was move, so nothing happens.
 
-    makes_copy(x);                  // x would move into the function, but
-                                    // type i32 implements the copy trait ...
-                                    // ... and x is thus still valid here
+fn gives_ownership() -> String {        // gives_ownership will move its
+                                        // return value into the function
+                                        // that calls it
+    let some_string = String::from("hello");    // some_string comes into scope
 
-}   // Here, x goes out of scope first, then s. But because the value of s was
-    // moved, nothing special happens.
+    some_string                         // some_string is returned and moves
+                                        // moves out to the calling function
+}
 
-fn takes_ownership(some_string: String) {  // some_string comes into scope
-    println!("{}", some_string);
-}   // Here, some_string goes out of scope and 'drop' is called.
-    // The backing memory is freed.
+// takes_and_gives_back will take a String and return one
+fn takes_and_gives_back(a_string: String) -> String {   // a_string comes into scope
 
-fn makes_copy(some_integer: i32) {          // some_integer comes into scope
-    println!("{}", some_integer);
-}   // Here, some_integer goes out of scope and nothing special happens.
+    a_string    // a_string is returned and moves out to the calling function
+}
