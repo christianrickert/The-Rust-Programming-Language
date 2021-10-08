6421854e7a0d35d9bd3b8019f13ec9ad5829f018
Christian Rickert <rc.email@icloud.com>
2021-10-07 21:06:29 -0600

    Listing 4-7

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,9 +1,14 @@
 fn main() {
-   let mut s = String::from("hello");
-
-   change(&mut s);
 }
 
-fn change(some_string: &mut String) {
-    some_string.push_str(", world");
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
 }
