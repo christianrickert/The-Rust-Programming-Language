7637466e02bf5cd4b6598c4a8f4133ced84010ac
Christian Rickert <rc.email@icloud.com>
2021-10-07 21:11:28 -0600

    Listing 4-8

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,4 +1,12 @@
 fn main() {
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s);  // word will get the value 5
+
+    s.clear();  // this empties the String, making it equal to ""
+
+    // word still has the value 5 here, but there's no more string that
+    // we could meaningfully use the value 5 with. word is now invalid.
 }
 
 fn first_word(s: &String) -> usize {
