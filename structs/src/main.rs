08c67e06f377a448cd857c657eebc85f9f31f4ed
Christian Rickert <rc.email@icloud.com>
2021-10-11 21:07:02 -0600

    Listing 5-6

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -5,18 +5,18 @@ struct User {
     active: bool,
 }
 
-fn build_user(email: String, username: String) -> User {
-    User {
-        email,
-        username,
+fn main() {
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
         active: true,
         sign_in_count: 1,
-    }
-}
+    };
 
-fn main() {
-    let user1 = build_user(
-        String::from("someone@example.com"),
-        String::from("someusername123"),
-    );
+    let user2 = User {
+        email: String::from("another@example.com"),
+        username: String::from("anotherusername567"),
+        active: user1.active,
+        sign_in_count: user1.sign_in_count,
+    };
 }
