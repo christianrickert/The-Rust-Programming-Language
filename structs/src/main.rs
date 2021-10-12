599dcb27c621d7b65ce1fbf5aa999a6d397feb5b
Christian Rickert <rc.email@icloud.com>
2021-10-11 20:51:04 -0600

    LIsting 5-3

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,15 +1,17 @@
-fn main() {
-    let user1 = User {
-        email: String::from("someone@example.com"),
-        username: String::from("someusername1234"),
-        active: true,
-        sign_in_count: 1,
-    };
-}
-
 struct User {
     username: String,
     email: String,
     sign_in_count: u64,
     active: bool,
+}
+
+fn main() {
+    let mut user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername1234"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("anotheremail@examplecom");
 }
