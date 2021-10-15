5968c5d0409130808a71f960230908c1a401873c
Christian Rickert <rc.email@icloud.com>
2021-10-14 21:36:25 -0600

    Listing 5-13

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,11 +4,20 @@ struct Rectangle {
     width: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.height * self.width
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         height: 30,
         width: 50,
     };
 
-    println!("rect1 is {:?}", rect1);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
 }
