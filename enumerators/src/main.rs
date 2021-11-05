57ee8c466a187bf7ee6d523849cb07ded8a2a6f3
Christian Rickert <rc.email@icloud.com>
2021-11-04 20:32:31 -0600

    Listing 6-2

--- enumerators/src/main.rs
+++ enumerators/src/main.rs
@@ -1,21 +1,7 @@
-fn main() {
-    enum IpAddrKind {
-        V4,
-        V6,
-    }
-
-    struct IpAddr {
-        kind: IpAddrKind,
-        address: String,
-    }
-
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
-
-    let home = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
 }
+fn main() {}
