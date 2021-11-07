486654444a453513f0cc03e26295a159e9373a70
Christian Rickert <rc.email@icloud.com>
2021-11-07 14:16:19 -0700

    Listing 6-3

--- enumerators/src/main.rs
+++ enumerators/src/main.rs
@@ -1,7 +1,17 @@
-enum Message {
-    Quit,
-    Move { x: i32, y: i32 },
-    Write(String),
-    ChangeColor(i32, i32, i32),
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
 }
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
 fn main() {}
