06145cd8b813a0ae775533929de98c66168c1b3e
Christian Rickert <rc.email@icloud.com>
2021-11-07 14:56:14 -0700

    Listing 6-4

--- enumerators/src/main.rs
+++ enumerators/src/main.rs
@@ -1,8 +1,15 @@
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -10,8 +17,13 @@ fn value_in_cents(coin: Coin) -> u8 {
         Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
     }
 }
 
-fn main() {}
+fn main() {
+    value_in_cents(Coin::Quarter(UsState::Alaska));
+}
