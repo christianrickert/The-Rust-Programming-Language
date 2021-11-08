53cf71bf2b773a959703cb2f925ff2ed24339e9b
Christian Rickert <rc.email@icloud.com>
2021-11-07 19:55:42 -0700

    code cleanup

--- enumerators/src/main.rs
+++ enumerators/src/main.rs
@@ -1,10 +1,9 @@
 fn main() {
-    //    let config_max = Some(3u8);
-    //    match config_max {
-    //        Some(max) => println!("The maximum is configured to be {}", max),
-    //        _ => (), // tuple
-    //    }
     let config_max = Some(3u8);
+    // match config_max {
+    //     Some(max) => println!("The maximum is configured to be {}", max),
+    //     _ => (), // tuple
+    // }
     if let Some(max) = config_max {
         println!("The maximum is configured to be {}", max);
     }
