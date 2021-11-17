48f9e154369ac0c51043b60731213e993beff623
Christian Rickert <rc.login@icloud.com>
2021-11-16 20:18:06 -0700

    Listing 7-16

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,13 +1,12 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitinglist() {}
-    }
+use std::fmt::Result;
+use std::io::Result as IoResult;
+
+fn function1() -> Result {
+    // --snip--
+    Ok(())
 }
 
-use self::front_of_house::hosting;
-
-pub fn eat_at_restaurant() {
-    hosting::add_to_waitinglist();
-    hosting::add_to_waitinglist();
-    hosting::add_to_waitinglist();
+fn function2() -> IoResult<()> {
+    // --snip--
+    Ok(())
 }
