0729a41610a38b244736c33d4ee6b8e09c3d5f07
Christian Rickert <rc.login@icloud.com>
2021-11-18 21:06:17 -0700

    Listing 8-10

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,7 +1,13 @@
 fn main() {
-    let mut v = vec![100, 32, 57];
-    for i in &mut v {
-        *i += 50;
-        println!("{}", i);
+    enum SpreadSheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
     }
+
+    let row = vec![
+        SpreadSheetCell::Int(3),
+        SpreadSheetCell::Text(String::from("blue")),
+        SpreadSheetCell::Float(10.12),
+    ];
 }
