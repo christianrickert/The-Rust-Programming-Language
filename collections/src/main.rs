6419fbf9ef225ca73e2a9ce0fb211eb316c71fcd
Christian Rickert <rc.login@icloud.com>
2021-11-22 21:56:36 -0700

    Listing 8-11

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,13 +1,3 @@
 fn main() {
-    enum SpreadSheetCell {
-        Int(i32),
-        Float(f64),
-        Text(String),
-    }
-
-    let row = vec![
-        SpreadSheetCell::Int(3),
-        SpreadSheetCell::Text(String::from("blue")),
-        SpreadSheetCell::Float(10.12),
-    ];
+    let mut s = String::new();
 }
