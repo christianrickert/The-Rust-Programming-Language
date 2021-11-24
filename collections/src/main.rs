a713b421a22049c84cfeeb5fdd0f1dfc4f72b783
Christian Rickert <rc.login@icloud.com>
2021-11-23 20:06:59 -0700

    Listing 8-18

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,4 +1,6 @@
 fn main() {
-    let mut s = String::from("lo");
-    s.push('l');
+    let mut s1 = String::from("Hello, ");
+    let mut s2 = String::from("world!");
+    let s3 = s1 + &s2; // noote s1 has been moved here
+                       // and can no longer be used
 }
