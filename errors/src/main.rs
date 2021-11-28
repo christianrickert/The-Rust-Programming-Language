a2bf3b83f5b7c94486f093d5fed0229294f4749a
Christian Rickert <rc.login@icloud.com>
2021-11-27 20:22:46 -0700

    Listing 9-6

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,14 +1,21 @@
-use std::fs::File;
-use std::io::ErrorKind;
-
+#![allow(unused)]
 fn main() {
-    let f = File::open("hello.txt").unwrap_or_else(|error| {
-        if error.kind() == ErrorKind::NotFound {
-            File::create("hello.txt").unwrap_or_else(|error| {
-                panic!("Problem creating the file: {:?}", error);
-            })
-        } else {
-            panic!("Problem opening the file: {:?}", error);
+    use std::fs::File;
+    use std::io::{self, Read};
+
+    fn read_username_from_file() -> Result<String, io::Error> {
+        let f = File::open("hello.txt");
+
+        let mut f = match f {
+            Ok(file) => file,
+            Err(e) => return Err(e),
+        };
+
+        let mut s = String::new();
+
+        match f.read_to_string(&mut s) {
+            Ok(_) => Ok(s),
+            Err(e) => Err(e),
         }
-    });
+    }
 }
