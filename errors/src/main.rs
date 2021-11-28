c9fe9fc3909f4ee2fff8c3ae5a022af3372b672f
Christian Rickert <rc.login@icloud.com>
2021-11-27 20:32:44 -0700

    Listing 9-8

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -5,9 +5,9 @@ fn main() {
     use std::io::Read;
 
     fn read_username_from_file() -> Result<String, io::Error> {
-        let mut f = File::open("hello.txt")?;
         let mut s = String::new();
-        f.read_to_string(&mut s)?;
+        File::open("hello.txt")?.read_to_string(&mut s)?;
+
         Ok(s)
     }
 }
