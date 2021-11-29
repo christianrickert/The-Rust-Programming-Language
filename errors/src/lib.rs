1faff3edfb86aca904f779860bccbf11e8eccdbb
Christian Rickert <rc.login@icloud.com>
2021-11-28 19:26:33 -0700

    Listing 10.17

--- errors/src/lib.rs
+++ errors/src/lib.rs
@@ -1,23 +1,12 @@
-fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
-    let mut largest = list[0];
-
-    for &item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
-}
-
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+    {
+        let r;
 
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
+        {
+            let x = 5;
+            r = &x;
+        }
 
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest(&char_list);
-    println!("The largest char is {}", result);
+        println!("r: {}", r);
+    }
 }
