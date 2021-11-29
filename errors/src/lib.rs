213a7edf9925fc439be9ee526c6df4f5965584bf
Christian Rickert <rc.login@icloud.com>
2021-11-28 19:30:53 -0700

    Listing 10-19

--- errors/src/lib.rs
+++ errors/src/lib.rs
@@ -1,12 +1,7 @@
 fn main() {
     {
-        let r;
-
-        {
-            let x = 5;
-            r = &x;
-        }
-
+        let x = 5;
+        let r = &x;
         println!("r: {}", r);
     }
 }
