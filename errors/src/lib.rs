c48cdf8f6ee002772e2a2a1902a639da8e8e3f9a
Christian Rickert <rc.login@icloud.com>
2021-11-30 20:05:36 -0700

    Listing 11-5

--- errors/src/lib.rs
+++ errors/src/lib.rs
@@ -1,12 +1,11 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn exploration() {
-        assert_eq!(2 + 2, 4);
-    }
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 
-    #[test]
-    fn another() {
-        panic!("Make this test fail");
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
     }
 }
