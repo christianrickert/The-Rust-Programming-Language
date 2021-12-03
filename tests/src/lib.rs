8b92a65fea54cf6c15ed380af82441a3833fc38d
Christian Rickert <rc.login@icloud.com>
2021-12-02 19:33:06 -0700

    Listing 11-7

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,8 +1,13 @@
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
 #[cfg(test)]
 mod tests {
+    use super:::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
     }
 }
