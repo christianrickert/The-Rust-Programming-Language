7c5363e1bdbcaffd9d8593f555b0489e5c7b1fa3
Christian Rickert <rc.login@icloud.com>
2021-12-02 19:47:54 -0700

    Listing 11-8

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,13 +1,24 @@
-pub fn add_two(a: i32) -> i32 {
-    a + 2
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be set between 1 and 100, got {}.", value);
+        }
+
+        Guess(value)
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super:::*;
+    use super::*;
 
     #[test]
-    fn it_adds_two() {
-        assert_eq!(4, add_two(2));
+    #[should_panic]
+    fn greater_than_100() {
+        Guess::new(200);
     }
 }
