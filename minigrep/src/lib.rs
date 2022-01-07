051fe3639db45d5abca12ed4ae227ae7af1544f2
Christian Rickert <rc.login@icloud.com>
2022-01-06 20:01:34 -0700

    Update lib.rs

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -8,7 +8,6 @@ pub struct Config {
 
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &str> {
-        // --snip--
         if args.len() < 3 {
             return Err("not enough arguments");
         }
@@ -21,10 +20,7 @@ impl Config {
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    // --snip--
     let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
 
     Ok(())
 }
@@ -49,9 +45,9 @@ mod tests {
     fn one_result() {
         let query = "duct";
         let contents = "\
-        Rust:
-        safe, fast, productive.
-        Pick three.";
+Rust:
+safe, fast, productive.
+Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
