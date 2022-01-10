2d719a9213bcd9cae91e29bd904953be7cdb7e33
Christian Rickert <rc.login@icloud.com>
2022-01-09 20:13:59 -0700

    Listing 13-3

--- closures_iterators/src/main.rs
+++ closures_iterators/src/main.rs
@@ -7,9 +7,31 @@ fn simulated_expensive_calculation(intensity: u32) -> u32 {
     intensity
 }
 
+fn generated_workout(intensity: u32, random_number: u32) {
+    if intensity < 25 {
+        println!(
+            "Today, do {} pushups!",
+            simulated_expensive_calculation(intensity)
+        );
+        println!(
+            "Today, do {} situps!",
+            simulated_expensive_calculation(intensity)
+        );
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run {} minutes!",
+                simulated_expensive_calculation(intensity)
+            );
+        }
+    }
+}
+
 fn main() {
     let simulated_user_specified_value = 10;
     let simulated_random_number = 7;
 
-    generated_workout(simulated_user_specified_value, simulated_random_number0;
+    generated_workout(simulated_user_specified_value, simulated_random_number);
 }
