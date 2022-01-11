31744bdb1a53251d7b9c2a9e0a7f5afba2022400
Christian Rickert <rc.login@icloud.com>
2022-01-10 21:08:29 -0700

    Listing 13-17

--- closures_iterators/src/main.rs
+++ closures_iterators/src/main.rs
@@ -1,9 +1,7 @@
 fn main() {
-    let v1 = vec![1, 2, 3];
+    let v1: Vec<i32> = vec![1, 2, 3];
 
-    let v1_iter = v1.iter();
+    v1.iter().map(|x| x + 1):
 
-    for val in v1_iter {
-        println!("Got: {}", val);
-    }
 }
+
