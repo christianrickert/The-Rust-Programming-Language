d194406190ac191de0564c71a457dde2038c9a57
Christian Rickert <rc.login@icloud.com>
2022-01-11 21:24:25 -0700

    Listing 14-3

--- cargo/src/lib.rs
+++ cargo/src/lib.rs
@@ -1,18 +1,30 @@
-//! # My Crate
+//! # Art
 //!
-//! `cargo` is a collection of utilities to make performing certain
-//! calculations more convenient.
+//! A library for modeling artistic concepts.
 
-/// Adds one to the number given.
-///
-/// # Examples
-///
-/// ```
-/// let arg = 5;
-/// let answer = cargo::add_one(arg);
-///
-/// assert_eq!(6, answer);
-/// ```
-pub fn add_one(x: i32) -> i32 {
-    x + 1
+pub mod kinds {
+    /// The primary colors according to the RYB color model.
+    pub enum PrimaryColor {
+        Red,
+        Yellow,
+        Blue,
+    }
+
+    /// The secondary colors according to the RYB color model.
+    pub enum SecondaryColor {
+        Orange,
+        Green,
+        Purple,
+    }
+}
+
+pub mod utils {
+    use crate::kinds::*;
+
+    /// Combines two primary colors in equal amounts to create
+    /// a secondary color.
+    pub fn mix(c1: PrimaryColor, c2: PrimaryColor) -> SecondaryColor {
+        // --snip--
+        unimplemented!();
+    }
 }
