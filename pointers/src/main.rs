e4ffaed731fe96fcb3ca9028fd1f9d8388f065d7
Christian Rickert <rc.login@icloud.com>
2022-01-22 21:50:20 -0700

    Listing 15-10

--- pointers/src/main.rs
+++ pointers/src/main.rs
@@ -1,3 +1,13 @@
+use std::ops::Deref;
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
 struct MyBox<T>(T);
 
 impl<T> MyBox<T> {
@@ -6,4 +16,10 @@ impl<T> MyBox<T> {
     }
 }
 
-fn main() {}
+fn main() {
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
