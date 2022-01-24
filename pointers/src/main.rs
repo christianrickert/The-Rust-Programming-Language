f28201e8f0ab79902eaabd06add9a3f3b7e64712
Christian Rickert <rc.email@icloud.com>
2022-01-23 21:45:50 -0700

    Listing 15-12

--- pointers/src/main.rs
+++ pointers/src/main.rs
@@ -3,7 +3,7 @@ use std::ops::Deref;
 impl<T> Deref for MyBox<T> {
     type Target = T;
 
-    fn deref(&self) -> &Self::Target {
+    fn deref(&self) -> &T {
         &self.0
     }
 }
@@ -16,10 +16,11 @@ impl<T> MyBox<T> {
     }
 }
 
-fn main() {
-    let x = 5;
-    let y = MyBox::new(x);
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
+}
 
-    assert_eq!(5, x);
-    assert_eq!(5, *y);
+fn main() {
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
 }
