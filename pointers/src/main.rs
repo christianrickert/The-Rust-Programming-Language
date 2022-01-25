0bbb8d10a58027b90b4cb3de6819357752503202
Christian Rickert <rc.email@icloud.com>
2022-01-24 20:34:44 -0700

    Listing 15-14

--- pointers/src/main.rs
+++ pointers/src/main.rs
@@ -1,26 +1,19 @@
-use std::ops::Deref;
-
-impl<T> Deref for MyBox<T> {
-    type Target = T;
-
-    fn deref(&self) -> &T {
-        &self.0
-    }
+struct CustomSmartPointer {
+    data: String,
 }
 
-struct MyBox<T>(T);
-
-impl<T> MyBox<T> {
-    fn new(x: T) -> MyBox<T> {
-        MyBox(x)
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data '{}'!", self.data);
     }
-}
-
-fn hello(name: &str) {
-    println!("Hello, {}!", name);
 }
 
 fn main() {
-    let m = MyBox::new(String::from("Rust"));
-    hello(&m);
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointers created.");
 }
