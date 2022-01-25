a734a4ff2f90451f699ce4a19b2c48593cd218ad
Christian Rickert <rc.email@icloud.com>
2022-01-24 20:45:32 -0700

    Listing 15-18

--- pointers/src/main.rs
+++ pointers/src/main.rs
@@ -1,19 +1,13 @@
-struct CustomSmartPointer {
-    data: String,
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
 }
 
-impl Drop for CustomSmartPointer {
-    fn drop(&mut self) {
-        println!("Dropping CustomSmartPointer with data '{}'!", self.data);
-    }
-}
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
 
 fn main() {
-    let c = CustomSmartPointer {
-        data: String::from("my stuff"),
-    };
-    let d = CustomSmartPointer {
-        data: String::from("other stuff"),
-    };
-    println!("CustomSmartPointers created.");
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let b = Cons(3, Rc::clone(&a));
+    let c = Cons(4, Rc::clone(&a));
 }
