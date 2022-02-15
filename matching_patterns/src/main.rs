c0a6d313cecb2710c32668bf4222100c8c0fee84
Christian Rickert <rc.login@icloud.com>
2022-02-14 20:04:01 -0700

    Listing 18-16

--- matching_patterns/src/main.rs
+++ matching_patterns/src/main.rs
@@ -1,23 +1,26 @@
+enum Color {
+    Rgb(i32, i32, i32),
+    Hsv(i32, i32, i32),
+}
+
 enum Message {
     Quit,
     Move { x: i32, y: i32 },
     Write(String),
-    ChangeColor(i32, i32, i32),
+    ChangeColor(Color),
 }
 
 fn main() {
-    let msg = Message::ChangeColor(0, 160, 255);
+    let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
 
     match msg {
-        Message::Quit => {
-            println!("The Quit variant has no data to destructure.")
-        }
-        Message::Move { x, y } => {
-            println!("Move in the x direction {} and in the y direction {}", x, y);
-        }
-        Message::Write(text) => println!("Text message: {}", text),
-        Message::ChangeColor(r, g, b) => {
+        Message::ChangeColor(Color::Rgb(r, g, b)) => {
             println!("Change the color to red {}, green {}, and blue {}", r, g, b)
         }
+        Message::ChangeColor(Color::Hsv(h, s, v)) => println!(
+            "Change the color to hue {}, saturation {}, and value {}",
+            h, s, v
+        ),
+        _ => (),
     }
 }
