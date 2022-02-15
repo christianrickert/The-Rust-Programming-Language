70958ef72c5df1905808a5c5a4dcf7c37240fc8c
Christian Rickert <rc.login@icloud.com>
2022-02-14 20:12:36 -0700

    Listing 18-27

--- matching_patterns/src/main.rs
+++ matching_patterns/src/main.rs
@@ -1,9 +1,12 @@
 fn main() {
-    let num = Some(4);
+    let x = Some(5);
+    let y = 10;
 
-    match num {
-        Some(x) if x < 5 => println!("less than five: {}", x),
-        Some(x) => println!("{}", x),
-        None => (),
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(n) if n == y => println!("Matched, n = {}", n),
+        _ => println!("Default case, x = {:?}", x),
     }
+
+    println!("at the end: x = {:?}, y = {}", x, y);
 }
