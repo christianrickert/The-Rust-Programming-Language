efbfbe7cdbebdd9016a41fb5c8f384f01b5ab503
Christian Rickert <rc.login@icloud.com>
2022-02-14 19:20:07 -0700

    Listing 18-4

--- matching_patterns/src/main.rs
+++ matching_patterns/src/main.rs
@@ -1,7 +1,3 @@
 fn main() {
-    let v = vec!['a', 'b', 'c'];
-
-    for (index, value) in v.iter().enumerate() {
-        println!("{} is at index {}", value, index);
-    }
+    let (x, y, z) = (1, 2, 3);
 }
