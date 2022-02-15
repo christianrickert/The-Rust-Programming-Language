a1ec50cb17ba3075cb05bb06062793513a39e56b
Christian Rickert <rc.login@icloud.com>
2022-02-14 20:15:02 -0700

    Listing 18-29

--- matching_patterns/src/main.rs
+++ matching_patterns/src/main.rs
@@ -1,9 +1,17 @@
 fn main() {
-    let x = 4;
-    let y = false;
+    enum Message {
+        Hello { id: i32 },
+    }
 
-    match x {
-        4 | 5 | 6 if y => println!("yes"),
-        _ => println!("no"),
+    let msg = Message::Hello { id: 5 };
+
+    match msg {
+        Message::Hello {
+            id: id_variable @ 3..=7,
+        } => println!("Found an id in range: {}", id_variable),
+        Message::Hello { id: 10..=12 } => {
+            println!("Found an id in another range")
+        }
+        Message::Hello { id } => println!("Found some other id: {}", id),
     }
 }
