10a8b23e08d7bd9d9030ba15ac176762e1354d9a
Christian Rickert <rc.login@icloud.com>
2022-02-14 19:26:40 -0700

    Listing 18-7

--- matching_patterns/src/main.rs
+++ matching_patterns/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     let (x, y, z) = (1, 2, 3);
+
+    fn foo(x: i32) {
+        // code goes here
+    }
+
+    fn print_coordinates(&(x, y): &(i32, i32)) {
+        println!("Current location: ({}, {})", x, y);
+    }
+    let point = (3, 5);
+    print_coordinates(&point);
 }
