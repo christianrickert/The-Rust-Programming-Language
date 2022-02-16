81f2e061029ea1757a4b9b7fa6cd60e99113e164
Christian Rickert <rc.login@icloud.com>
2022-02-15 20:39:28 -0700

    Listing 19-22

--- advanced/src/main.rs
+++ advanced/src/main.rs
@@ -1,21 +1,15 @@
-trait Animal {
-    fn baby_name() -> String;
-}
+use std::fmt;
 
-struct Dog;
-
-impl Dog {
-    fn baby_name() -> String {
-        String::from("Spot")
+trait OutlinePrint: fmt::Display {
+    fn outline_print(&self) {
+        let output = self.to_string();
+        let len = output.len();
+        println!("{}", "*".repeat(len + 4));
+        println!("*{}*", " ".repeat(len + 2));
+        println!("* {} *", output);
+        println!("*{}*", " ".repeat(len + 2));
+        println!("{}", "*".repeat(len + 4));
     }
 }
 
-impl Animal for Dog {
-    fn baby_name() -> String {
-        String::from("puppy")
-    }
-}
-
-fn main() {
-    println!("A baby dog is called a {}", Dog::baby_name());
-}
+fn main() {}
