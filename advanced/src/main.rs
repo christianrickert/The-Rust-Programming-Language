513acc86844ce51118cee62c99733bdd37424c09
Christian Rickert <rc.login@icloud.com>
2022-02-15 20:40:23 -0700

    Listing 19-23

--- advanced/src/main.rs
+++ advanced/src/main.rs
@@ -1,15 +1,14 @@
 use std::fmt;
 
-trait OutlinePrint: fmt::Display {
-    fn outline_print(&self) {
-        let output = self.to_string();
-        let len = output.len();
-        println!("{}", "*".repeat(len + 4));
-        println!("*{}*", " ".repeat(len + 2));
-        println!("* {} *", output);
-        println!("*{}*", " ".repeat(len + 2));
-        println!("{}", "*".repeat(len + 4));
+struct Wrapper(Vec<String>);
+
+impl fmt::Display for Wrapper {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[{}]", self.0.join(", "))
     }
 }
 
-fn main() {}
+fn main() {
+    let w = Wrapper(vec![String::from("hello"), String::from("world")]);
+    println!("w = {}", w);
+}
