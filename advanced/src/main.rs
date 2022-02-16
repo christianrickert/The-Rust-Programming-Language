2b0dbce94c1d354b883aac024b898b49233fa2ca
Christian Rickert <rc.login@icloud.com>
2022-02-15 20:35:57 -0700

    Listing 19-16

--- advanced/src/main.rs
+++ advanced/src/main.rs
@@ -1,25 +1,29 @@
-use std::ops::Add;
-
-#[derive(Debug, Copy, Clone, PartialEq)]
-struct Point {
-    x: i32,
-    y: i32,
+trait Pilot {
+    fn fly(&self);
 }
 
-impl Add for Point {
-    type Output = Point;
+trait Wizard {
+    fn fly(&self);
+}
 
-    fn add(self, other: Point) -> Point {
-        Point {
-            x: self.x + other.x,
-            y: self.y + other.y,
-        }
+struct Human;
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("This is your captain speaking.");
     }
 }
 
-fn main() {
-    assert_eq!(
-        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
-        Point { x: 3, y: 3 }
-    );
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("Up!");
+    }
 }
+
+impl Human {
+    fn fly(&self) {
+        println!("*waving arms furiously*");
+    }
+}
+
+fn main() {}
