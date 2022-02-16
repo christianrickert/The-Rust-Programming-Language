d106487fdbf288a57854c028412b9e6dcdec9f0d
Christian Rickert <rc.login@icloud.com>
2022-02-15 20:28:53 -0700

    Listing 19-11

--- advanced/src/main.rs
+++ advanced/src/main.rs
@@ -1,15 +1,9 @@
-static mut COUNTER: u32 = 0;
-
-fn add_to_count(inc: u32) {
-    unsafe {
-        COUNTER += inc;
-    }
+unsafe trait Foo {
+    // methods go here
 }
 
-fn main() {
-    add_to_count(3);
-
-    unsafe {
-        println!("COUNTER: {}", COUNTER);
-    }
+unsafe impl Foo for i32 {
+    // method implementations go here
 }
+
+fn main() {}
