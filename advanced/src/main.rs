5774992f0205992038a540569a7741ad4d45a7ec
Christian Rickert <rc.login@icloud.com>
2022-02-15 20:22:20 -0700

    Listing 19-8

--- advanced/src/main.rs
+++ advanced/src/main.rs
@@ -1,20 +1,9 @@
-use std::slice;
-
-fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
-    let len = slice.len();
-    let ptr = slice.as_mut_ptr();
-
-    assert!(mid <= len);
-
-    unsafe {
-        (
-            slice::from_raw_parts_mut(ptr, mid),
-            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
-        )
-    }
+extern "C" {
+    fn abs(input: i32) -> i32;
 }
 
 fn main() {
-    let mut vector = vec![1, 2, 3, 4, 5, 6];
-    let (left, right) = split_at_mut(&mut vector, 3);
+    unsafe {
+        println!("Absolute value of -3 according to C: {}", abs(-3));
+    }
 }
